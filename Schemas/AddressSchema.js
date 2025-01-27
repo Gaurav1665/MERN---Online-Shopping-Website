9d525eeb2ca50bdd604e6167f426061635730e68
Ida Gaurav <127031558+Gaurav1665@users.noreply.github.com>
2025-01-27 05:55:31 +0530

    Adding schemas of project

---
+++ Schemas/AddressSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const schema = mongoose.Schema({
+  AddressId: Number,
+  Address: String,
+  City:String,
+  State:String,
+  Country:String,
+  Pincode:Number,
+  UserId:Number
+})
+
+module.exports = mongoose.model('address', schema)
